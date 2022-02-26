7ff035f45c9442040f57a78fe537074cb63373e1
Vadim Suraev <Xhaklaaa@users.noreply.github.com>
2022-02-26 08:35:56 -0500

    reFilling chessboard in the main.c

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-
 int main()
 {
-	int i, j;
-	char chess[9][9]
-		= {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-		{'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{'6', '_', '_', '_', '_', '_', '_', '_', '_'},
-		{'5', '_', '_', '_', '_', '_', '_', '_', '_'},
-		{'4', '_', '_', '_', '_', '_', '_', '_', '_'},
-		{'3', '_', '_', '_', '_', '_', '_', '_', '_'},
-		{'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-		{' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-		printf("\n");
-	for (i = 0; i < 9; i++) {
-		for (j = 0; j < 9; j++) {
-		printf("%c", chess[i][j]);
-		}
-	printf("\n");
-	}
+	printf("8 r n b q k b n r\n");
+	printf("7 p p p p p p p p\n");
+	printf("6\n");
+	printf("5\n");
+	printf("4\n");
+	printf("3\n");
+	printf("2 P P P P P P P P\n");
+	printf("1 R N B Q K B N R\n");
+	printf(" a b c d e f g h\n");
 	return 0;
 }
+

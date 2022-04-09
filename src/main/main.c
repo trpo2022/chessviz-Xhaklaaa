94a59f55c5df083cad926607d2aa70004503f9e8
Vadim Suraev <Xhaklaaa@users.noreply.github.com>
2022-04-09 09:50:43 -0400

    Making a pawn movement

---
+++ src/main/main.c
@@ -0,0 +1,40 @@
+#include <lib/chessviz.h>
+
+int w_pass = 0; 
+int w_x = -1;   
+int b_pass = 0; 
+int b_x = -1;  
+
+int main()
+{
+    char board[8][8];
+    int MoveNum = 0;
+    Create(board);
+    Show(board);
+    int n = 0;
+    char w[7];
+    char b[7];
+    while (n == MoveNum) {
+        MoveNum += 1;
+        scanf("%d. ", &n);
+        if (MoveNum == n) {
+            scanf("%s", w);
+            if (WinW(board, w, n)) {
+                break;
+            }
+            scanf("%s", b);
+            if (WinB(board, w, b, n)) {
+                break;
+            }
+            printf("\n%d. %s %s \n", n, w, b);
+            White(board, w);
+            w_pass = 0;
+            Black(board, b);
+            b_pass = 0;
+        } else {
+            Error();
+            break;
+        }
+    }
+    return 0;
+}

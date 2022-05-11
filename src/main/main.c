b965422015d49933260d185acfc75181300643de
Vadim Suraev <Xhaklaaa@users.noreply.github.com>
2022-05-11 12:32:09 -0400

    Update codestyle

--- src/main/main.c
+++ src/main/main.c
@@ -11,49 +11,39 @@ int main()
     int MoveNum = 0;
     Create(board);
     Show(board);
-    int n=0;
+    int n = 0;
     char w[7];
     char b[7];
-    while(n==MoveNum){
+    while (n == MoveNum) {
         MoveNum += 1;
-        scanf("%d. ",&n);
-        if(MoveNum==n){
-            scanf("%s",w);
-            if(WinW(board,w,n)){
+        scanf("%d. ", &n);
+        if (MoveNum == n) {
+            scanf("%s", w);
+            if (WinW(board, w, n)) {
                 break;
             }
-            scanf("%s",b);
-            if(WinB(board,w,b,n)){
+            scanf("%s", b);
+            if (WinB(board, w, b, n)) {
                 break;
             }
-            printf("\n%d. %s %s \n",n,w,b);
-            White(board,w);
-            if(CheckW(board)){
+            printf("\n%d. %s %s \n", n, w, b);
+            White(board, w);
+            if (CheckW(board)) {
                 ErrorC(2);
                 break;
             }
-            w_pass=0;
-            Black(board,b);
-            if(CheckB(board)){
+            w_pass = 0;
+            Black(board, b);
+            if (CheckB(board)) {
                 ErrorC(2);
                 break;
             }
-            b_pass=0;
-        }else{
+            b_pass = 0;
+        } else {
             ErrorC(1);
             break;
         }
     }
 
-    
-          
-            
-    
-
-          
-    
-    
-  
     return 0;
 }
-        
--- test/main.c
+++ test/main.c
@@ -4,9 +4,9 @@
 
 // Constant
 int w_pass = 0; // White
-int w_x = -1;   
+int w_x = -1;
 int b_pass = 0; // Black
-int b_x = -1;   
+int b_x = -1;
 
 int main(int argc, const char** argv)
 {
